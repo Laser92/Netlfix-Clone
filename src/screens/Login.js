14bea66625b1847a911ead4e6a0e21fbfc042d16
Laser92 <avikrdas92@gmail.com>
2023-04-20 18:36:20 +0530

    login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import SignUpScreen from "./SignUpScreen";
 
 function Login() {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="background">
@@ -10,12 +12,35 @@ function Login() {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        <button
-          className="button"
-          // onClick={}
-        >
+        <button className="button" onClick={() => setSignIn(true)}>
           Sign in
         </button>
+        <div className="gradient" />
+        <div className="body">
+          {signIn ? (
+            <SignUpScreen />
+          ) : (
+            <>
+              <h1>Unlimited files, TV programmes and more.</h1>
+              <h2>Watch anywhere. Cancel at any time.</h2>
+              <h3>
+                Ready to watch? Enter your email to create or restart your
+                membership{" "}
+              </h3>
+              <div className="logininput">
+                <form>
+                  <input type="email" placeholder="Email Address" />
+                  <button
+                    className="getStarted"
+                    onClick={() => setSignIn(true)}
+                  >
+                    GET STARTED{" "}
+                  </button>
+                </form>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./SignUpScreen.css";
+
+function SignUpScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="gray">New to Netflix?</span>
+          <span className="link" onClick={register}>Sign Up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpScreen;
