be59bce41d43daeb895ba9e38b3bdc8446f21753
Laser92 <avikrdas92@gmail.com>
2023-04-20 19:02:27 +0530

    no one will ever see this :)

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./SignUpScreen.css";
 
 function SignUpScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
   const register = (e) => {
     e.preventDefault();
-  };
-  const signIn = (e) => {
-    e.preventDefault();
+    //   auth.createUserWithEmailAndPassword(
+    //     emailRef.current.value,passwordRef.current.value
+    //   )
+    //   .then((authUser)=>{
+    //     console.log(authUser)})
+    //     .catch(error=>{alert(error.message)
+    //     }); 
+    //   };
+    // const signIn = (e) => {
+    //   e.preventDefault();
   };
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passwordRef} placeholder="Password" type="password" />
         <button type="submit">Sign In</button>
         <h4>
           <span className="gray">New to Netflix?</span>
-          <span className="link" onClick={register}>Sign Up now.</span>
+          <span className="link" onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
