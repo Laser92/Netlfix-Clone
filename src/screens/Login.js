909e0fbced6c54005723fd499007a430c439e1b9
Laser92 <avikrdas92@gmail.com>
2023-04-26 14:08:27 +0530

    FE Done

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -24,13 +24,13 @@ function Login(props) {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
-        {/* {signUp ? ( */}
-        <button className="button" onClick={() => setSignUp(true)}>
-          Sign in
-        </button>
-        {/* ) : ( */}
-        {/* <></> */}
-        {/* )} */}
+        {!signUp ? (
+          <button className="button" onClick={() => setSignUp(true)}>
+            Sign in
+          </button>
+        ) : (
+          <></>
+        )}
 
         <div className="gradient" />
         <div className="body">
